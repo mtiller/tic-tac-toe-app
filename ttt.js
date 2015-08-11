0b7813289bbbaedc4bd98ae27dd567d2a6ac8674
Michael Tiller <michael.tiller@gmail.com>
2015-08-11 16:11:02 -0400

    Added a button to change the board
    This adds a function that I can invoke with the button.  The function **changes the state
    of the board**.  This demonstrates the first step in interactivity.

--- ttt.js
+++ ttt.js
@@ -3,10 +3,15 @@ console.log("When you loaded this page, you ran something too!")
 var ttt = angular.module("TTT", []);
 
 ttt.controller("GameCtrl", ["$scope", function($scope) {
-	this.user = "Player";
-	this.board = [
+	var game = this;
+	game.user = "Player";
+	game.board = [
 		["X", "X", "O"],
 		["O", "", "O"],
 		["X", "O", "X"]
 	]
+
+	game.move = function() {
+		game.board[1][1] = "X";
+	};
 }]);
