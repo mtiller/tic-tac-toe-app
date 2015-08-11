9492751e2e66a438773323fc51ba2de608bfe7d7
Matthew Pelukas <mjpelukas@yahoo.com>
2015-08-11 15:38:43 -0400

    Array board rendered

--- ttt.js
+++ ttt.js
@@ -3,5 +3,9 @@ console.log("When you loaded this page, you ran something too!")
 var ttt = angular.module("TTT", []);
 
 ttt.controller("GameCtrl", ["$scope", function($scope) {
-	this.user = "Matthew";
+	this.user = "Player";
+	var Leftcolumn=["X","X","O"]
+	var Centercolumn=["O","X","O"]
+	var rightcolumn=["X","O","X"]
+	this.board=[Leftcolumn, Centercolumn, rightcolumn]
 }]);
