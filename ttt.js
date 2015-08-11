c5d6734813ab3ec174e2ef5b408d96eac42dc5ff
Michael Tiller <michael.tiller@gmail.com>
2015-08-11 15:58:22 -0400

    Inlined the definition of the board
    This makes it a bit easier to read.

--- ttt.js
+++ ttt.js
@@ -4,8 +4,9 @@ var ttt = angular.module("TTT", []);
 
 ttt.controller("GameCtrl", ["$scope", function($scope) {
 	this.user = "Player";
-	var Leftcolumn=["X","X","O"]
-	var Centercolumn=["O","X","O"]
-	var rightcolumn=["X","O","X"]
-	this.board=[Leftcolumn, Centercolumn, rightcolumn]
+	this.board = [
+		["X", "X", "O"],
+		["O", "", "O"],
+		["X", "O", "X"]
+	]
 }]);
